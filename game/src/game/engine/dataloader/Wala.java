36c32483f140c914891a0286c0a07ea45db5cf10
mohamedAyman58 <mohamedaymanoman.1@gmail.com>
2026-03-12 23:54:29 +0200

    Update Wala.java

--- game/src/game/engine/dataloader/Wala.java
+++ game/src/game/engine/dataloader/Wala.java
@@ -3,5 +3,5 @@ package game.engine.dataloader;
 public class Wala {
  String wala="walalaawlala5555";
  String doma = "  el wad el 8areeeeeeeeeeeeb     البياخدطكككككك";
- 
+ String alakyasta="jkasyrtuiewr t";
 }
