98ba73549825e0102d272e05ee7910442a0ea12f
HamzaEllaithy <7amza.ellaithy@gmail.com>
2026-03-14 18:03:32 +0200

    ay haga
    ah haga

--- game/src/game/engine/dataloader/Wala.java
+++ game/src/game/engine/dataloader/Wala.java
@@ -1,7 +1,5 @@
 package game.engine.dataloader;
 
 public class Wala {
- String wala="walalaawlala5555";
- String doma = "  el wad el 8areeeeeeeeeeeeb     البياخدطكككككك";
- String alakyasta="jkasyrtuiewr t";
+String ayhaga = "ay haga";
 }
